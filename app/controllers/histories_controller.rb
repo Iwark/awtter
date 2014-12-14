651ccd6bee6b55d3a19e2c8c7590bac09b1f6463
Iwark <iwark02@gmail.com>
2014-12-14 17:27:00 +0900

    history and error

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -2,9 +2,14 @@ class HistoriesController < ApplicationController
 
   # 一覧
   def index
-    lg = IO.readlines("log/crontab.log")
-    @log_lines = lg
-    @log_lines = lg[-100..-1] if lg.length > 100
+    lg = IO.readlines("log/history.log")
+    @histories = lg
+    @histories = lg[-100..-1] if lg.length > 100
+
+    lg = IO.readlines("log/error.log")
+    @errors = lg
+    @errors = lg[-100..-1] if lg.length > 100
+
   end
 
 end
