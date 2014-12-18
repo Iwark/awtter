0d227b972adf52a519d8807fb71d747d9ca7e8ab
iwasaki.kohei <iwasaki.kohei@donuts.ne.jp>
2014-12-18 13:00:00 +0900

    aaa

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -12,7 +12,7 @@ class HistoriesController < ApplicationController
 
     @chart_data = {}
     PowerHistory.where("created_at > ?", 7.days.ago).group(:created_at).sum(:followers_sum).each do |k, v|
-      key = k.to_date.strftime("%m/%d")
+      key = k.to_date.strftime("%m%d")
       @chart_data[key] = v
     end
   end
