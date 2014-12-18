166f7f916d695af15cf85d2137463c5dbe0f60eb
iwasaki.kohei <iwasaki.kohei@donuts.ne.jp>
2014-12-18 12:10:51 +0900

    chart data

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -9,6 +9,16 @@ class HistoriesController < ApplicationController
     lg = IO.readlines("log/error.log")
     @errors = lg
     @errors = lg[-100..-1] if lg.length > 100
+
+    @chart_data = {}
+    PowerHistory.where("created_at > ?", 7.days.ago).group(:created_at).sum(:followers_sum).each do |k, v|
+      key = k.to_date.strf_time("%m/%d %H:%M")
+      if @chart_data[key]
+        @chart_data[key] += v
+      else
+        @chart_data[key] = v
+      end
+    end
   end
 
 end
