ca5e4605342f981b0fe79c4243f8805a860e9474
Iwark <iwark02@gmail.com>
2014-12-20 23:10:34 +0900

    user

--- app/models/account.rb
+++ app/models/account.rb
@@ -144,9 +144,10 @@ class Account < ActiveRecord::Base
     follower_ids.each do |follower_id|
       if !FollowedUser.exists?(user_id: follower_id, account_id: self.id) && !friend_ids.include?(follower_id)
         if i < 3
-          user = get_user(client, follower_id)
-          FollowedUser.create(user_id: follower_id, name: user.screen_name, account_id: self.id, status:"follower", checked: true)
-          i += 1
+          if user = get_user(client, follower_id)
+            FollowedUser.create(user_id: follower_id, name: user.screen_name, account_id: self.id, status:"follower", checked: true)
+            i += 1
+          end
         end
       end
     end
